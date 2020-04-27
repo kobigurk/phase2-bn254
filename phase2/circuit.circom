f0ebf4ee75a1203bd5186975f21b8e72d8383dc8
Kobi Gurkan <kobigurk@gmail.com>
2020-04-27 14:22:31 +0300

    adds check_ratio tests and modifies tests to accomodate it

--- phase2/circuit.circom
+++ phase2/circuit.circom
@@ -1,11 +1,13 @@
 template Num2Bits(n) {
     signal input in;
+    signal intermediate[n];
     signal output out[n];
     var lc1=0;
 
     for (var i = 0; i<n; i++) {
         out[i] <-- (in >> i) & 1;
         out[i] * (out[i] -1 ) === 0;
+        intermediate[i] <== out[i] * out[i];
         lc1 += out[i] * 2**i;
     }
 
--- powersoftau/test.sh
+++ powersoftau/test.sh
@@ -24,5 +24,4 @@ cargo run --release --bin verify_transform_constrained challenge3 response3 chal
 cargo run --release --bin beacon_constrained challenge4 response4 $SIZE $BATCH 0000000000000000000a558a61ddc8ee4e488d647a747fe4dcc362fe2026c620
 cargo run --release --bin verify_transform_constrained challenge4 response4 challenge5 $SIZE $BATCH
 
-cat response1 response2 response3 response4 > transcript
-cargo run --release --bin verify  transcript $SIZE $BATCH
+cargo run --release --bin prepare_phase2 response4 $SIZE $BATCH

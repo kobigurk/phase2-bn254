765e6c7004eea83c280201b79f84b547bb22397b
Jordi Baylina <jordi@baylina.cat>
2020-04-29 22:56:00 +0200

    Circom 0.5 binary formats support

--- phase2/src/bin/new.rs
+++ phase2/src/bin/new.rs
@@ -5,14 +5,19 @@ extern crate exitcode;
 use std::fs::File;
 use phase2::parameters::MPCParameters;
 use phase2::circom_circuit::circuit_from_json_file;
+use phase2::circom_circuit::circuit_from_r1cs_file;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 4 {
-        println!("Usage: \n<in_circuit.json> <out_params.params> <path/to/phase1radix>");
+        println!("Usage: \n<in_circuit.<r1cs|json>> <out_params.params> <path/to/phase1radix>");
         std::process::exit(exitcode::USAGE);
     }
     let circuit_filename = &args[1];
+    let circuit_filename_ext = match std::path::Path::new(circuit_filename).extension() {
+        Some(os) => os.to_str().unwrap(),
+        None => ""
+    };
     let params_filename = &args[2];
     let radix_directory = &args[3];
 
@@ -21,7 +26,11 @@ fn main() {
     // Import the circuit and create the initial parameters using phase 1
     println!("Creating initial parameters for {}...", circuit_filename);
     let params = {
-        let c = circuit_from_json_file(&circuit_filename);
+        let c = if circuit_filename_ext.eq_ignore_ascii_case("JSON") {
+            circuit_from_json_file(&circuit_filename)
+        } else {
+            circuit_from_r1cs_file(&circuit_filename)
+        };
         MPCParameters::new(c, should_filter_points_at_infinity, radix_directory).unwrap()
     };
 
--- phase2/src/bin/prove.rs
+++ phase2/src/bin/prove.rs
@@ -15,13 +15,15 @@ use phase2::circom_circuit::{
     create_rng,
     proof_to_json_file,
     circuit_from_json_file,
-    witness_from_json_file
+    circuit_from_r1cs_file,
+    witness_from_json_file,
+    witness_from_wtns_file
 };
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 6 {
-        println!("Usage: \n<circuit.json> <witness.json> <params> <proof.json> <public.json>");
+        println!("Usage: \n<circuit.<r1cs|json>> <witness.<wtns|json>> <params> <proof.json> <public.json>");
         std::process::exit(exitcode::USAGE);
     }
     let circuit_filename = &args[1];
@@ -29,11 +31,27 @@ fn main() {
     let params_filename = &args[3];
     let proof_filename = &args[4];
     let public_filename = &args[5];
+    let circuit_filename_ext = match std::path::Path::new(circuit_filename).extension() {
+        Some(os) => os.to_str().unwrap(),
+        None => ""
+    };
+    let witness_filename_ext = match std::path::Path::new(witness_filename).extension() {
+        Some(os) => os.to_str().unwrap(),
+        None => ""
+    };
 
     let rng = create_rng();
     let params = load_params_file(params_filename);
-    let mut circuit = circuit_from_json_file(circuit_filename);
-    circuit.witness =  Some(witness_from_json_file::<Bn256>(witness_filename));
+    let mut circuit = if circuit_filename_ext.eq_ignore_ascii_case("JSON") {
+        circuit_from_json_file(&circuit_filename)
+    } else {
+        circuit_from_r1cs_file(&circuit_filename)
+    };
+    circuit.witness = if witness_filename_ext.eq_ignore_ascii_case("JSON") {
+        Some(witness_from_json_file::<Bn256>(&witness_filename))
+    } else {
+        Some(witness_from_wtns_file::<Bn256>(&witness_filename))
+    };
 
     println!("Proving...");
     let proof = prove(circuit.clone(), &params, rng).unwrap();

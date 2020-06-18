1baf99888f4b74e2a692cff9d03db533e789716f
Jordi Baylina <jordi@baylina.cat>
2020-06-18 09:57:59 +0200

    Change multiplicative generator of bn254 to 5 instead of 7

--- pairing/src/bn256/fr.rs
+++ pairing/src/bn256/fr.rs
@@ -2,7 +2,7 @@ use ff::{Field, PrimeField, PrimeFieldRepr};
 
 #[derive(PrimeField)]
 #[PrimeFieldModulus = "21888242871839275222246405745257275088548364400416034343698204186575808495617"]
-#[PrimeFieldGenerator = "7"]
+#[PrimeFieldGenerator = "5"]
 pub struct Fr(FrRepr);
 
 #[test]
